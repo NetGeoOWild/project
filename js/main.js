846c79eff76e3ccc1b0f04d64c3b9c22c9d7a5d2
George <example@gmail.com>
2022-08-17 18:58:25 +0300

    java script home-work done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+const numberOfFilms = Number(prompt("Сколько фильмов вы уже посмотрели?"));
+const oneOfTheLastFilm = prompt("Один из последних просмотренных фильмов?");
+const filmGrade = prompt("На сколько вы его оцените?");
 
-});
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+
+personalMovieDB["movies"][oneOfTheLastFilm] = filmGrade;
